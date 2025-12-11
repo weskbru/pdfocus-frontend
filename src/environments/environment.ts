b752dcfa3eb0afe2dde3d785439004b105b229f3
Your Name <you@example.com>
2025-12-10 22:55:42 -0300

    fix(cors)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // Este é o ficheiro de PRODUÇÃO (usado pelo Vercel)
 export const environment = {
   production: true,
-  apiUrl: 'https://pdfocus-backend-s184.onrender.com' // Aponta para o Backend em produção
+  apiUrl: 'https://pdfocus-oz7g.onrender.com' // Aponta para o Backend em produção
 };
